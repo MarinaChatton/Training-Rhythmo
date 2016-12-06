afcf100db73ac6396e3cd3bda73ecd0f3229ceb3
Marina Chatton <marina.chatton@gmail.com>
2016-12-06 16:44:04 +0100

    ADD metronome
    added metronome

--- app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
+++ app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
@@ -1,22 +1,53 @@
 package com.chatton.marina.rhythmo;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.chatton.marina.rhythmo.metronome.Metronome;
 import com.lukedeighton.wheelview.WheelView;
 
 public class MainActivity extends AppCompatActivity {
+    //views
     WheelView wheelView;
     TextView rpmDisplay;
+    TextView speedDisplay;
+    FloatingActionButton onOffButton;
+
+    //metronome
+    Metronome metronome;
+    boolean isPlaying = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        metronome = new Metronome();
+
         rpmDisplay = (TextView) findViewById(R.id.display_rpm);
+        speedDisplay = (TextView) findViewById(R.id.display_speed);
+
+        onOffButton = (FloatingActionButton) findViewById(R.id.on_off_button);
+        onOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPlaying){
+                    metronome.stop();
+                    isPlaying = false;
+                }else{
+                    metronome.play();
+                    isPlaying = true;
+                }
+            }
+        });
 
         wheelView = (WheelView) findViewById(R.id.wheelview);
         wheelView.setOnWheelAngleChangeListener(new WheelView.OnWheelAngleChangeListener() {
@@ -26,4 +57,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
