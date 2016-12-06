1ecb538bf3aae66ab7bc15ee0a52f3c396cc29f8
Marina Chatton <marina.chatton@gmail.com>
2016-12-06 20:14:07 +0100

    UPDATE UIstyle

--- app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
+++ app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.chatton.marina.rhythmo;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
@@ -13,7 +15,7 @@ import android.widget.TextView;
 import com.chatton.marina.rhythmo.metronome.Metronome;
 import com.lukedeighton.wheelview.WheelView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, WheelView.OnWheelAngleChangeListener {
     //views
     WheelView wheelView;
     TextView rpmDisplay;
@@ -32,31 +34,47 @@ public class MainActivity extends AppCompatActivity {
 
         metronome = new Metronome();
 
+        wheelView = (WheelView) findViewById(R.id.wheelview);
+        wheelView.setOnWheelAngleChangeListener(this);
+
         rpmDisplay = (TextView) findViewById(R.id.display_rpm);
         speedDisplay = (TextView) findViewById(R.id.display_speed);
 
         onOffButton = (FloatingActionButton) findViewById(R.id.on_off_button);
-        onOffButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isPlaying){
-                    metronome.stop();
-                    isPlaying = false;
-                }else{
-                    metronome.play();
-                    isPlaying = true;
-                }
-            }
-        });
-
-        wheelView = (WheelView) findViewById(R.id.wheelview);
-        wheelView.setOnWheelAngleChangeListener(new WheelView.OnWheelAngleChangeListener() {
-            @Override
-            public void onWheelAngleChange(float angle) {
-                rpmDisplay.setText(String.valueOf(angle));
-            }
-        });
+        onOffButton.setOnClickListener(this);
+        setOnOffButtonColors(isPlaying);
     }
 
+    private void setOnOffButtonColors(boolean isPlaying){
+        int iconColor;
+        int backgroundColor;
+        if(isPlaying){
+            iconColor = getResources().getColor(android.R.color.holo_blue_bright);
+            backgroundColor = getResources().getColor(R.color.colorPrimary);
+        }else{
+            iconColor = getResources().getColor(R.color.colorDark);
+            backgroundColor = getResources().getColor(R.color.colorPrimary);
+        }
+        onOffButton.getDrawable().setColorFilter(iconColor, PorterDuff.Mode.SRC_IN);
+        onOffButton.setBackgroundTintList(ColorStateList.valueOf(backgroundColor));
+    }
 
+    //onOffButtonClickListener
+    @Override
+    public void onClick(View v) {
+        if(isPlaying){
+            //metronome.stop();
+            isPlaying = false;
+        }else{
+            //metronome.play();
+            isPlaying = true;
+        }
+        setOnOffButtonColors(isPlaying);
+    }
+
+    //onWheelviewAngleChangeListener
+    @Override
+    public void onWheelAngleChange(float angle) {
+        rpmDisplay.setText(String.valueOf(angle));
+    }
 }
