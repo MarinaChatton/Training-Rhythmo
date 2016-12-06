fdc8608fe6962884e1c4295c33f007147feaea2a
Marina Chatton <marina.chatton@gmail.com>
2016-12-06 14:21:01 +0100

    ADD wheelView
    added wheelview and textiew displaying the angle of the wheel

--- app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
+++ app/src/main/java/com/chatton/marina/rhythmo/MainActivity.java
@@ -2,12 +2,28 @@ package com.chatton.marina.rhythmo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.lukedeighton.wheelview.WheelView;
 
 public class MainActivity extends AppCompatActivity {
+    WheelView wheelView;
+    TextView angleDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        angleDisplay = (TextView) findViewById(R.id.angle);
+
+        wheelView = (WheelView) findViewById(R.id.wheelview);
+        wheelView.setOnWheelAngleChangeListener(new WheelView.OnWheelAngleChangeListener() {
+            @Override
+            public void onWheelAngleChange(float angle) {
+                angleDisplay.setText(String.valueOf(angle));
+            }
+        });
     }
 }
